8efa716e64a457d923a3e1979a31111628710860
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-05-21 23:51:47 +0800

    加载package

---
+++ Assets/My/Test02/FairyGUITest02.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FairyGUI;
+using UnityEngine;
+
+public class FairyGUITest02 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GRoot.inst.SetContentScaleFactor(1920, 1080);
+        UIPackage.AddPackage("FGUI/Package1");
+        GComponent component = UIPackage.CreateObject("Package1", "Bg3").asCom;
+        GRoot.inst.AddChild(component);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
