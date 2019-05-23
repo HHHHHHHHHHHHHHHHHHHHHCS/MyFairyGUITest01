0dd8fe1b11228eefb1c3e981fb1ae9e87a964306
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-05-23 23:32:31 +0800

    and scirpt and anim

--- Assets/My/03_Boss/FGUIBoss.cs
+++ Assets/My/03_Boss/FGUIBoss.cs
@@ -7,19 +7,27 @@ public class FGUIBoss : MonoBehaviour
 {
     private GComponent mainUI;
     private GComponent bossCom;
+    private GGroup group;
 
     private void Start()
     {
         mainUI = GetComponent<UIPanel>().ui;
+        group = mainUI.GetChild("n2").asGroup;
         bossCom = UIPackage.CreateObject("Package2", "Boss").asCom;
         mainUI.GetChild("n0").onClick.Add(() => PlayBossButton(bossCom));
+
     }
 
     private void PlayBossButton(GComponent targetCom)
     {
-        mainUI.GetChild("n0").visible = false;
+        group.visible = false;
         GRoot.inst.AddChild(targetCom);
         Transition t = targetCom.GetTransition("t0");
-        t.Play();
+        t.Play(() =>
+        {
+            group.visible=true;
+            GRoot.inst.RemoveChild(targetCom);
+        });
+
     }
 }
