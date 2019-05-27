c7a9a63b6ec36a5b8f9c23b8ea9f280057a4bc15
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-05-28 00:05:33 +0800

    06 progress bar

---
+++ Assets/My/06_ProgressBar/TestProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class TestProgressBar : MonoBehaviour
+{
+    private GComponent mainUI;
+    private GProgressBar progressBar;
+    private GComboBox comboBox;
+
+    private void Start()
+    {
+        mainUI = GetComponent<UIPanel>().ui;
+        progressBar = mainUI.GetChild("n3").asProgress;
+        progressBar.TweenValue(100, 5);
+        comboBox = mainUI.GetChild("n2").asComboBox;
+        comboBox.onChanged.Add(SetCompleteTime);
+
+    }
+
+    private void SetCompleteTime()
+    {
+        progressBar.value = 0;
+        progressBar.TweenValue(100, Convert.ToInt32(comboBox.value));
+    }
+}
