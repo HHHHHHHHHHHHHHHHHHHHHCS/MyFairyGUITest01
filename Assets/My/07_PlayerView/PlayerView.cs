abe113046d2b3f121ef6dc7307246f003472ad6b
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-06-02 00:13:44 +0800

    add player view

---
+++ Assets/My/07_PlayerView/PlayerView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FairyGUI;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    private GComponent mainUI;
+    private PlayerWindow playerWindow;
+
+    private void Start()
+    {
+        mainUI = GetComponent<UIPanel>().ui;
+        playerWindow = new PlayerWindow();
+        mainUI.GetChild("n0").onClick.Add(() => { playerWindow.Show(); });
+    }
+}
