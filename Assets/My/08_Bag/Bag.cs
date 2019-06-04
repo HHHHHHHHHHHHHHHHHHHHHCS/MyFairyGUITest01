2ed594c2b01d4d1b33b3ed072404f4259d9b1258
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-06-04 23:23:26 +0800

    bag  window

---
+++ Assets/My/08_Bag/Bag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FairyGUI;
+using UnityEngine;
+
+public class Bag : MonoBehaviour
+{
+    private GComponent mainUI;
+    private GButton playerView;
+    private MyBagWindow bagWindow;
+
+    private void Start()
+    {
+        mainUI = GetComponent<UIPanel>().ui;
+        playerView = mainUI.GetChild("playerView").asButton;
+        playerView.onClick.Add(UseItem);
+        bagWindow = new MyBagWindow();
+        //bagWindow.SetXY();
+        mainUI.GetChild("bagButton").onClick.Add(bagWindow.Show);
+    }
+
+    private void UseItem()
+    {
+
+    }
+}
---
+++ Assets/My/08_Bag/MyBagWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FairyGUI;
+using UnityEngine;
+
+public class MyBagWindow : Window
+{
+    protected override void OnInit()
+    {
+        this.contentPane = UIPackage.CreateObject("08_Bag", "BagWindow").asCom;
+    }
+}
